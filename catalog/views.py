e922cb68c2160a5527dff4fb38e4e2dfe646301c
Preston Lam <plam@resource-innovations.com>
2024-04-27 22:01:47 -0600

    added new stuff to view.py

--- catalog/views.py
+++ catalog/views.py
@@ -7,12 +7,16 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.all().count()
+    num_genres = Genre.objects.all().count()
+    num_books_selected = Book.objects.filter(title__istartswith="the").count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_selected': num_books_selected,
     }
 
     return render(request, 'index.html', context=context)
